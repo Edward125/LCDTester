0ac137978d4bb02d8ba9b8260858e8e64df6c3cb
Edward <edward_song@yeah.net>
2019-05-31 13:06:44 +0800

    add lcd demo

--- LCDTester/frmMain.Designer.cs
+++ LCDTester/frmMain.Designer.cs
@@ -28,21 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.lblMini = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabMain = new System.Windows.Forms.TabControl();
+            this.tabCamera = new System.Windows.Forms.TabPage();
+            this.tabDebug = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.txtImgFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreateRoi = new System.Windows.Forms.Button();
+            this.panelTitle.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabMain.SuspendLayout();
+            this.tabDebug.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelTitle.Controls.Add(this.lblClose);
+            this.panelTitle.Controls.Add(this.lblMini);
+            this.panelTitle.Controls.Add(this.lblTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1024, 38);
+            this.panelTitle.TabIndex = 0;
+            this.panelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseDown);
+            // 
+            // lblClose
+            // 
+            this.lblClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblClose.AutoSize = true;
+            this.lblClose.ForeColor = System.Drawing.Color.White;
+            this.lblClose.Location = new System.Drawing.Point(955, 9);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(16, 17);
+            this.lblClose.TabIndex = 2;
+            this.lblClose.Text = "X";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            this.lblClose.MouseLeave += new System.EventHandler(this.lblClose_MouseLeave);
+            this.lblClose.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblClose_MouseMove);
+            // 
+            // lblMini
+            // 
+            this.lblMini.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMini.AutoSize = true;
+            this.lblMini.ForeColor = System.Drawing.Color.White;
+            this.lblMini.Location = new System.Drawing.Point(918, 8);
+            this.lblMini.Name = "lblMini";
+            this.lblMini.Size = new System.Drawing.Size(18, 17);
+            this.lblMini.TabIndex = 1;
+            this.lblMini.Text = "__";
+            this.lblMini.Click += new System.EventHandler(this.lblMini_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(43, 17);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "label1";
+            this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.tabMain);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1024, 730);
+            this.panel1.TabIndex = 1;
+            // 
+            // tabMain
+            // 
+            this.tabMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabMain.Controls.Add(this.tabCamera);
+            this.tabMain.Controls.Add(this.tabDebug);
+            this.tabMain.Location = new System.Drawing.Point(0, 0);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(1024, 730);
+            this.tabMain.TabIndex = 0;
+            // 
+            // tabCamera
+            // 
+            this.tabCamera.Location = new System.Drawing.Point(4, 26);
+            this.tabCamera.Name = "tabCamera";
+            this.tabCamera.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCamera.Size = new System.Drawing.Size(1016, 700);
+            this.tabCamera.TabIndex = 0;
+            this.tabCamera.Text = "Camera";
+            this.tabCamera.UseVisualStyleBackColor = true;
+            // 
+            // tabDebug
+            // 
+            this.tabDebug.Controls.Add(this.splitContainer1);
+            this.tabDebug.Location = new System.Drawing.Point(4, 26);
+            this.tabDebug.Name = "tabDebug";
+            this.tabDebug.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDebug.Size = new System.Drawing.Size(1016, 700);
+            this.tabDebug.TabIndex = 1;
+            this.tabDebug.Text = "Debug";
+            this.tabDebug.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.hWindowControl1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnCreateRoi);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.txtImgFile);
+            this.splitContainer1.Size = new System.Drawing.Size(1010, 694);
+            this.splitContainer1.SplitterDistance = 735;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(3, 0);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(729, 694);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(729, 694);
+            // 
+            // txtImgFile
+            // 
+            this.txtImgFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtImgFile.Location = new System.Drawing.Point(5, 32);
+            this.txtImgFile.Name = "txtImgFile";
+            this.txtImgFile.Size = new System.Drawing.Size(263, 23);
+            this.txtImgFile.TabIndex = 0;
+            this.txtImgFile.DoubleClick += new System.EventHandler(this.txtImgFile_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "圖像文件:";
+            // 
+            // btnCreateRoi
+            // 
+            this.btnCreateRoi.BackColor = System.Drawing.Color.Silver;
+            this.btnCreateRoi.FlatAppearance.BorderSize = 0;
+            this.btnCreateRoi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreateRoi.Location = new System.Drawing.Point(10, 61);
+            this.btnCreateRoi.Name = "btnCreateRoi";
+            this.btnCreateRoi.Size = new System.Drawing.Size(120, 31);
+            this.btnCreateRoi.TabIndex = 2;
+            this.btnCreateRoi.Text = "生成ROI";
+            this.btnCreateRoi.UseVisualStyleBackColor = false;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelTitle);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tabMain.ResumeLayout(false);
+            this.tabDebug.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.Label lblMini;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabMain;
+        private System.Windows.Forms.TabPage tabCamera;
+        private System.Windows.Forms.TabPage tabDebug;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.TextBox txtImgFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreateRoi;
     }
 }
 
--- LCDTester/frmMain.cs
+++ LCDTester/frmMain.cs
@@ -4,8 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
+using Edward;
 
 namespace LCDTester
 {
@@ -31,11 +33,25 @@ namespace LCDTester
         }
         #endregion
 
+        #region 界面移动
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_MOVE = 0xF010;
+        public const int HTCAPTION = 0x0002;
+        #endregion
+
+        private bool IsRuning = false;
+
 
         private void LoadUI()
         {
             this.WindowState = FormWindowState.Maximized;
-            this.WindowState = FormWindowState.Maximized;
+            this.Text = p.AppTitle;
+            lblTitle.Text = p.AppTitle;
+            txtImgFile.SetWatermark("雙擊此處選擇圖像文件.");
             p.CheckFolder();
             p.CreateIni();
             p.ReadIni();
@@ -45,5 +61,78 @@ namespace LCDTester
         {
             LoadUI();
         }
+
+        private void panelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+        }
+
+        private void lblTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            if (!IsRuning)
+            {
+                DialogResult dr = MessageBox.Show("是否確認退出軟件,退出點擊是(Y),不退出點擊否(N)?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    try
+                    {
+                        Environment.Exit(0);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
+                }
+            }
+        }
+
+        private void lblMini_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void lblClose_MouseLeave(object sender, EventArgs e)
+        {
+            lblClose.BackColor = Color.Transparent;
+        }
+
+        private void lblClose_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblClose.BackColor = Color.Red;
+        }
+
+        private void txtImgFile_DoubleClick(object sender, EventArgs e)
+        {
+
+            openfile(txtImgFile);
+
+        }
+
+
+        private void openfile(TextBox textbox)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textbox.Text = open.FileName;
+            }
+
+
+
+        }
+
+
+
+
+
+
     }
 }
